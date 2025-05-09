cae1dd25e84118dcd517e47ced9e6414fff65b08
vtmartin <v.martin.alonso@udc.es>
2025-05-09 18:14:42 +0200

    [PI-PrimeraIteracion FUNC-1] SearchFund funcional

--- stock-data/stockdata/settings.py
+++ stock-data/stockdata/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'searchFund.apps.SearchfundConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
