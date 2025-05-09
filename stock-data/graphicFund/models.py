8444be530cc7b620ff0ebcfd48d0000f4c71eb65
vtmartin <v.martin.alonso@udc.es>
2025-05-09 15:04:52 +0200

    [PI-PrimeraIteracion]Models de los fondos

--- stock-data/graphicFund/models.py
+++ stock-data/graphicFund/models.py
@@ -1,3 +1,14 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Fund(models.Model):
+    symbol = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+    sector = models.CharField(max_length=100, blank=True)
+    return1y = models.FloatField(null=True, blank=True, help_text="Rentabilidad en el último año")
+    fees = models.FloatField(null=True, blank=True, help_text="Comisiones anuales")
+    benchmark = models.CharField(max_length=100, blank=True)
+    volatility = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.symbol} - {self.name}"
